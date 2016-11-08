5720d265e912873b66a609ff4cfa7a5b826666d3
Conrad Davis Jr <conrad.davis@akqa.com>
2016-11-07 22:58:34 -0800

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// var css = require("!raw!sass!./file.scss");
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: './build.js'
+  },
+  watch: true,
+  // scripts: {
+  //   "test-watch": "webpack --config test/watch/webpack.config.js"
+  // },
+  module: {
+    preLoaders: [
+        {
+            test: /\.js$/, // include .js files
+            exclude: /node_modules/, // exclude any and all files in the node_modules folder
+            loader: "jshint-loader"
+        }
+    ],
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      },
+      {
+        test: /\.s$/,
+        loader: 'babel',
+        exclude: /node_modules$/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
