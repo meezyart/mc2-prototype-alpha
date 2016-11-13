11b952eb6dbc9f5b4719dd8a4f57e5b1ffd27293
Conrad Davis Jr <conrad.davis@akqa.com>
2016-11-13 15:46:40 -0800

    environment build

---
+++ scripts/overlay.js
@@ -0,0 +1,22 @@
+/*jshint esversion: 6 */
+// overlay.js
+// 
+// Author: Conrad Davis Jr
+// 
+// library for loading content into an overlay modal
+// 
+
+function nothing() {
+  var blah = 0;
+  // var Overlay = {
+
+  //   init: function(){
+  //     this.elements();
+  //   },
+
+  //   elements: function(){
+  //     var blah = document.querySelectorAll('.blah');
+  //   }
+  // };
+  // Overlay.init();
+};
