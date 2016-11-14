fe96596e02de55c182f21537254958eed1d9ab7a
Conrad Davis Jr <conrad.davis@akqa.com>
2016-11-13 23:31:22 -0800

    es6 code for overlay

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -8,15 +8,37 @@
 
 window.onload = () => {
 
-  const Overlay = {
+  // var RS_img_handler = {
 
-    init: function(){
+  //   init: function(){
+  //     this.setupVariables();
+  //     this.setupElements();
+  //     this.bpCheck();
+  //     this.eventHandlers();
+  //   },
+
+  var Overlay = {
+
+
+    init: function() {
+      console.log('this: ', this);
       this.elements();
     },
 
-    elements: function(){
-      var blah = document.querySelectorAll('.blah');
+    elements: function() {
+      this.overlayView = document.querySelectorAll('.overlay-view');
+    },
+
+    animate: ( animation ) => {
+      console.log('overlayView: ', Overlay.overlayView);
+      switch (animation){
+        case 'fromRightToLeft':
+          console.log("Logged fromRightToLeft");
+          break;
+      }
+      
     }
+
   };
   Overlay.init();
   window.Overlay = Overlay;
