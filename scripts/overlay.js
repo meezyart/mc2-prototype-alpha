5795870cbcc93f8d53af479debef245468bde090
Conrad Davis Jr <conrad.davis@akqa.com>
2016-11-16 21:55:50 -0800

    added overlay open close animatino logic

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -10,26 +10,48 @@ window.onload = () => {
 
   var Overlay = {
 
+    // init
     init: function() {
       this.state();
       this.elements();
+      this.events();
     },
-
+    // state
     state: function(){
-      this.animationCache = 0;
+      this.reverseAnimationCache = '';
     },
+    // elements
     elements: function() {
       this.overlayView = document.querySelectorAll('.overlay-view');
+      this.overlayCloseBtn = document.querySelectorAll('.overlay-view__close-btn');
     },
 
+    // event listeners
+    events: () => {
+      Overlay.overlayCloseBtn[0].onclick = () => {
+        Overlay.closeOverlay();
+      };
+    },
+
+    // Overlay animation handler
+    // PUBLIC METHODS
+    // 
     animate: ( animation ) => {
+      // clear all animation classes from the overlay
+
+      // animations based on overlay animation styles ex: 'from-right-to-left'
       Overlay.overlayView[0].classList.add(animation);
 
-      // store the most recently selected animation request
-      Overlay.animationCache = animation;
+      // store the most recently selected animation request, and reverse its direction
+      Overlay.reverseAnimationCache = animation + '-reversed';
     },
 
+    // handler for closing the overlay modal
     closeOverlay: () => {
+      Overlay.animate(Overlay.reverseAnimationCache);
+    },
+
+    animationsDirectory: () => {
 
     }
 
