35b76ebf7e57c36087b78b5c27484ca16fb86ecf
Conrad Davis Jr <conrad.davis@akqa.com>
2016-12-19 21:09:23 -0800

    setup linkning for jsx

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -2,11 +2,24 @@ const React = require('react')
 const { Link } = require('react-router')
 
 const Landing = () => (
-  <div className='app-container'>
-    <div className='home-info'>
-      <h1 className='title'>svideo</h1>
-      <input className='search' type='text' placeholder='Search' />
-      <Link to='/search' className='browse-all'> or Browse All</Link>
+  <div id="profilePage" className="is-hidden" pageItem>
+    <div className="profile-buttons">
+      <Link to='/search' className="profile-button" view-path="gaurdiansPage" navbutton>
+        <div className="profile-icon gaurdians"></div>
+        <div className="icon-name">Gaurdians</div>
+      </Link>
+      <div className="profile-button" view-path="childrenPage" navbutton>
+        <div className="profile-icon children"></div>
+        <div className="icon-name">Children</div>
+      </div>
+      <div className="profile-button" view-path="hostPage" navbutton>
+        <div className="profile-icon hosts"></div>
+        <div className="icon-name">Become a Host</div>
+      </div>
+      <div className="profile-button" view-path="gaurdiansPage" navbutton>
+        <div className="profile-icon browse-hosts"></div>
+        <div className="icon-name">Browse Hosts</div>
+      </div>
     </div>
   </div>
 )
