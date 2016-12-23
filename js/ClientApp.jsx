1537e3af5784f4d649ff2eb63b8743bcd2c485cf
Conrad Davis Jr <conrad.davis@akqa.com>
2016-12-23 12:55:40 -0800

    save gaurdian details

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,12 +2,14 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Gaurdians = require('./Gaurdians')
+const GaurdiansDetail = require('./GaurdiansDetail')
 const { Router, Route, hashHistory } = require('react-router')
 
 const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Landing} />
     <Route path='/gaurdians' component={Gaurdians} />
+    <Route path='/gaurdians-detail' component={GaurdiansDetail} />
   </Router>
 )
 
