094ea700a2a5cd50723b42eecd91f705e0ef7596
Conrad Davis Jr <conrad.davis@akqa.com>
2016-12-28 23:31:17 -0800

    added second data set in react

--- js/Gaurdians.jsx
+++ js/Gaurdians.jsx
@@ -1,27 +1,22 @@
 const React = require('react')
-const data = require('../public/data')
 const data2 = require('../public/mock-data')
 const { Link } = require('react-router')
 
 const Gaurdians = () => (
   <div id="gaurdiansPage" className="is-hidden" pageItem>
     <Link to='/gaurdians-detail' className="page-banner gaurdians-banner"><span></span>gaurdians</Link>
-    <section className="gaurdian-buttons"></section>
+    <section className="gaurdian-buttons">
+      {data2.gaurdians.map((gaurdian) => (
+        <div className="gaurdian-buttons__gaurdian-button" view-path="gaurdianDetails" navbutton>
+          <div className="gaurdian-buttons__profile-picture" style={{backgroundImage: gaurdian.profilePicture}}></div>
+          <div className="gaurdian-buttons__gaurdian-name">{gaurdian.firstName} {gaurdian.lastName}</div>
+          <div className="gaurdian-buttons__gaurdian-title">{gaurdian.gaurdianStatus}</div>
+        </div>
+      ))}
+    </section>
     <div className="gaurdian-buttons__gaurdian-button" navbutton>
       <div className="gaurdian-buttons__profile-picture add-more"></div>
       <div className="gaurdian-buttons__gaurdian-name">Add a Gaurdian</div>
-    </div>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <div className='show-card'>
-          <img src={`public/img/posters/${show.poster}`} className='show-card-img' />
-          <div className='show-card-text'>
-            <h3 className='show-card-title'>{show.title}</h3>
-            <h4 className='show-card-year'>({show.year})</h4>
-            <p className='show-card-description'>{show.description}</p>
-          </div>
-        </div>
-      ))}
     </div>
     <div className='shows'>
       {data2.gaurdians.map((gaurdian) => (
