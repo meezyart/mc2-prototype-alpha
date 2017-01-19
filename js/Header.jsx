27075357108f5c1787a9af3b7dcfab4c00052db0
Conrad Davis Jr <conrad.davis@akqa.com>
2017-01-18 21:23:40 -0800

    updated paths

--- js/Header.jsx
+++ js/Header.jsx
@@ -6,24 +6,24 @@ class Header extends React.Component {
     return (
       <div className="header">
 
-        <div className="header__logo header--button"><img src="images/logo.jpg" /></div>
+        <div className="header__logo header--button"><img src="public/images/logo.jpg" /></div>
 
         <div className="header__home-btn header--button" view-path="profilePage" headerbutton>
-          <img src="images/home-btn.jpg" />
+          <img src="public/images/home-btn.jpg" />
         </div>
 
         <div className="header__friends-btn header--button">
-          <img src="images/friends-btn.jpg" />
+          <img src="public/images/friends-btn.jpg" />
           <div className="header__items-pending-count">10</div>
         </div>
 
         <div className="header__notifications-btn header--button">
-          <img src="images/notifications-btn.jpg" />
+          <img src="public/images/notifications-btn.jpg" />
           <div className="header__items-pending-count is-hidden"></div>
         </div>
 
         <div className="header__additional-options-btn header--button">
-          <img src="images/header-btn.jpg" />
+          <img src="public/images/nav-btn.jpg" />
         </div>
       </div>
     )
--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,24 +1,28 @@
 const React = require('react')
+const Header = require('./Header')
 const { Link } = require('react-router')
 
 const Landing = () => (
-  <div id="profilePage" className="is-hidden" pageItem>
-    <div className="profile-buttons">
-      <Link to='/guardians' className="profile-button" view-path="guardiansPage" navbutton>
-        <div className="profile-icon guardians"></div>
-        <div className="icon-name">guardians</div>
-      </Link>
-      <div className="profile-button" view-path="childrenPage" navbutton>
-        <div className="profile-icon children"></div>
-        <div className="icon-name">Children</div>
-      </div>
-      <div className="profile-button" view-path="hostPage" navbutton>
-        <div className="profile-icon hosts"></div>
-        <div className="icon-name">Become a Host</div>
-      </div>
-      <div className="profile-button" view-path="guardiansPage" navbutton>
-        <div className="profile-icon browse-hosts"></div>
-        <div className="icon-name">Browse Hosts</div>
+  <div>
+    <Header />
+    <div id="profilePage" pageItem>
+      <div className="profile-buttons">
+        <Link to='/guardians' className="profile-button" view-path="guardiansPage" navbutton>
+          <div className="profile-icon guardians"></div>
+          <div className="icon-name">guardians</div>
+        </Link>
+        <div className="profile-button" view-path="childrenPage" navbutton>
+          <div className="profile-icon children"></div>
+          <div className="icon-name">Children</div>
+        </div>
+        <div className="profile-button" view-path="hostPage" navbutton>
+          <div className="profile-icon hosts"></div>
+          <div className="icon-name">Become a Host</div>
+        </div>
+        <div className="profile-button" view-path="guardiansPage" navbutton>
+          <div className="profile-icon browse-hosts"></div>
+          <div className="icon-name">Browse Hosts</div>
+        </div>
       </div>
     </div>
   </div>
