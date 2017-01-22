8df95156133996a0f35cd3b5b9aad8ad26f393eb
Conrad Davis Jr <conrad.davis@akqa.com>
2017-01-21 18:55:20 -0800

    successfully out put dynamic data with react and MC2 app data

--- js/Guardians.jsx
+++ js/Guardians.jsx
@@ -1,12 +1,11 @@
 const React = require('react')
-const data = require('../public/mock-data')
 const axios = require('axios')
 const Header = require('./Header')
 const Backbutton = require('./Backbutton')
 const { Link } = require('react-router')
 
 class Guardians extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
 
     this.state = {
@@ -15,9 +14,9 @@ class Guardians extends React.Component {
       error: null
     }
   }
-  componentWillMount() {
+  componentWillMount () {
     // Remove the 'www.' to cause a CORS error (and see the error state)
-    axios.get(`http://mycommunityclassroom.herokuapp.com/api/v1/user/583d2be3330b4500660b78e2/details`)
+    axios.get('http://mycommunityclassroom.herokuapp.com/api/v1/user/583d2be3330b4500660b78e2/details')
       .then(res => {
         // Transform the raw data by extracting the nested posts
         const posts = res.data
@@ -39,36 +38,6 @@ class Guardians extends React.Component {
         })
       })
   }
-  renderError() {
-    return (
-      <div>
-        Uh oh: {this.state.error.message}
-      </div>
-    )
-  }
-
-  renderPosts() {
-    if (this.state.error) {
-      return this.renderError()
-    }
-    var obj = {a:1, b:2, c:3}
-    
-    for (var prop in obj) {
-      console.log('obj.' + prop, '=', obj[prop])
-    }
-
-    var outPut = []
-
-    for (let item in this.state.posts){
-      let value = this.state.posts[item] 
-      outPut.push(<li>{`${item} ${value}`}</li>)
-    }
-    return (
-      <ul>
-        {outPut}
-      </ul>
-    )
-  }
   render () {
     return (
       <div>
@@ -76,15 +45,12 @@ class Guardians extends React.Component {
         <div id="guardians-page">
           <div className="page-banner guardians-banner"><Backbutton /><div className="icon"></div>guardians</div>
           <section className="guardian-buttons">
-            {data.guardians.map((guardian) => (
-              <Link to={`/guardians-detail/${guardian.id}`} className="guardian-buttons__guardian-button" key={guardian.id}>
-                <div className="guardian-buttons__profile-picture" style={{backgroundImage: guardian.profilePicture}}></div>
-                <div className="guardian-buttons__guardian-name">{guardian.firstName} {guardian.lastName}</div>
-                <div className="guardian-buttons__guardian-title">{guardian.guardianStatus}</div>
-              </Link>
-            ))}
+            <Link to={`/guardians-detail/${this.state.posts._id}`} className="guardian-buttons__guardian-button" key={this.state.posts._id}>
+              <div className="guardian-buttons__profile-picture" style={{backgroundImage: this.state.posts.profilePicture}}></div>
+              <div className="guardian-buttons__guardian-name">{this.state.posts.fName} {this.state.posts.lName}</div>
+              <div className="guardian-buttons__guardian-title">{this.state.posts.relationship}</div>
+            </Link>
           </section>
-          {this.renderPosts()}
           <div className="guardian-buttons__guardian-button">
             <div className="guardian-buttons__profile-picture add-more"></div>
             <div className="guardian-buttons__guardian-name">Add a guardian</div>
